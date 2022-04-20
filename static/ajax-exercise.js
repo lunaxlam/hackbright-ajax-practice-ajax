fd29600c094539c3b683eed6fc33f7ba150bfbf1
lunaxlam@gmail.com <lunalam@Lunas-MBP.attlocal.net>
2022-04-20 12:21:58 -0700

    Added comments

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,8 +3,11 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
+  // Go to this route and get the Response object
   fetch('/fortune')
+    // Convert the Response object into only text
     .then(response => response.text()
+    // Assign the text form of the Response object ('data) as the innerHTML
     .then(data => {
       // console.log(data)
       document.querySelector('#fortune-text').innerHTML = data
@@ -16,19 +19,25 @@ document.querySelector('#get-fortune-button').addEventListener('click', showFort
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
+  // Prevent the default HTML from rendering
   evt.preventDefault();
 
+  // Define the base URL for fetching
   const url = '/weather.json';
+  // Get the value we need for the query-string to concatenate to the base URL
   const zipcode = document.querySelector('#zipcode-field').value;
-  // weather.json?zipcode=98000
+  // Example of what the URL should look like: weather.json?zipcode=98000
+  // Create the query string with the given value and convert toString() if necessary
   const queryString = new URLSearchParams({zipcode}).toString();
-  // const url = '/weather.json${queryString}';
 
 
+  // Go to this URL and get the Response object
   fetch(`${url}?${queryString}`)
+    // Convert the response object into JSON
     .then(response => response.json())
     .then(data => {
       console.log(data)
+      // Set the innerHTML as the value at data['forecast']
       document.querySelector('#weather-info').innerHTML = data['forecast']
     })
 }
